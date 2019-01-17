3b666683e865dc2398533538d207e4a7a5326587
Lukas Bischofberger <me@worx.li>
2019-01-18 00:14:40 +0100

    test pipeline

--- vochabular/manage.py
+++ vochabular/manage.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vochabular.settings')
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line # type: ignore
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
