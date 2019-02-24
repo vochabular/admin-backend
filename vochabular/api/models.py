1df6de8fdd3415bbb238bd121d9b3c77da0098c6
Sumanthini Majutharan <sumanthini.majutharan@powercoders.org>
2019-02-24 14:09:43 +0100

    Create WordGroup Model
    * Set self Foreign key in Chapter Model
    * Create Component and ComponentType Model
    * Create Text and Translation Model
    * Create Comment Model
    * Create word, member and Word in different language Model
    * Made changes as per feedback
    * change the coding according to the feedback

--- vochabular/api/models.py
+++ vochabular/api/models.py
@@ -1,7 +1,124 @@
 from django.db import models
 
+
 class Chapter(models.Model):
     title = models.CharField(max_length=100)
+    belongs_to = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
 
-    def __str__(self):
-        return self.title
+    def __int__(self):
+        return self.id
+
+
+class ComponentType(models.Model):
+    name = models.CharField(max_length=45)
+    schema = models.TextField(max_length=100)
+
+    def __int__(self):
+        return self.id
+
+
+class Component(models.Model):
+    STATE_CHOICES = (
+        ('C', 'creation'),
+        ('R', 'in review'),
+        ('U', 'to be updated'),
+        ('T', 'to be translated'),
+        ('F', 'final')
+    )
+
+    data = models.TextField(max_length=500)
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+    componentType = models.ForeignKey(ComponentType, on_delete=models.CASCADE)
+    state = models.CharField(max_length=1, choices=STATE_CHOICES, default='0')
+
+    def __int__(self):
+        return self.id
+
+
+class Text(models.Model):
+    translatable = models.BooleanField()
+    component = models.ForeignKey(Component, on_delete=models.CASCADE)
+    master_translation = models.IntegerField()
+
+    def __int__(self):
+        return self.id
+
+
+class Translation(models.Model):
+    language = models.CharField(max_length=45)
+    text = models.CharField(max_length=45)
+    valid = models.BooleanField()
+    text_id = models.OneToOneField(Text, on_delete=models.CASCADE)
+
+    def __int__(self):
+        return self.id
+
+
+class Comment(models.Model):
+    comment = models.TextField(max_length=500)
+    active = models.BooleanField()
+    component = models.ForeignKey(Component, on_delete=models.CASCADE)
+
+    def __int__(self):
+        return self.id
+
+
+class WordGroup(models.Model):
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+
+    def __int__(self):
+        return self.id
+
+
+class Word(models.Model):
+
+    def __int__(self):
+        return self.id
+
+
+class Member(models.Model):
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    word_group = models.ForeignKey(WordGroup, on_delete=models.CASCADE)
+
+    def __int__(self):
+        return self.id
+
+
+class WordCH(models.Model):
+    text = models.CharField(max_length=40)
+    word = models.OneToOneField(Word, on_delete=models.CASCADE)
+
+    def __int__(self):
+        return self.id
+
+
+class WordEN(models.Model):
+    text = models.CharField(max_length=40)
+    word = models.OneToOneField(Word, on_delete=models.CASCADE)
+
+    def __int__(self):
+        return self.id
+
+
+class WordDE(models.Model):
+    text = models.CharField(max_length=40)
+    word = models.OneToOneField(Word, on_delete=models.CASCADE)
+
+    def __int__(self):
+        return self.id
+
+
+class WordFA(models.Model):
+    text = models.CharField(max_length=40)
+    word = models.OneToOneField(Word, on_delete=models.CASCADE)
+
+    def __int__(self):
+        return self.id
+
+
+class WordAR(models.Model):
+    text = models.CharField(max_length=40)
+    word = models.OneToOneField(Word, on_delete=models.CASCADE)
+
+    def __int__(self):
+        return self.id
--- vochabular/api/schema.py
+++ vochabular/api/schema.py
@@ -1,18 +1,165 @@
 import graphene
 from graphql_jwt.decorators import login_required
-
 from graphene_django.types import DjangoObjectType
-from api.models import Chapter
+from api.models import (
+    Chapter,
+    ComponentType,
+    Component,
+    Text,
+    Translation,
+    Comment,
+    WordGroup,
+    Word,
+    Member,
+    WordCH,
+    WordEN,
+    WordDE,
+    WordFA,
+    WordAR
+)
+
 
 class ChapterType(DjangoObjectType):
     class Meta:
         model = Chapter
 
+
+class ComponentTypeType(DjangoObjectType):
+    class Meta:
+        model = ComponentType
+
+
+class Component_Type(DjangoObjectType):
+    class Meta:
+        model = Component
+
+
+class TextType(DjangoObjectType):
+    class Meta:
+        model = Text
+
+
+class TranslationType(DjangoObjectType):
+    class Meta:
+        model = Translation
+
+
+class CommentType(DjangoObjectType):
+    class Meta:
+        model = Comment
+
+
+class WordGroupType(DjangoObjectType):
+    class Meta:
+        model = WordGroup
+
+
+class WordType(DjangoObjectType):
+    class Meta:
+        model = Word
+
+
+class MemberType(DjangoObjectType):
+    class Meta:
+        model = Member
+
+
+class WordCHType(DjangoObjectType):
+    class Meta:
+        model = WordCH
+
+
+class WordENType(DjangoObjectType):
+    class Meta:
+        model = WordEN
+
+
+class WordDEType(DjangoObjectType):
+    class Meta:
+        model = WordDE
+
+
+class WordFAType(DjangoObjectType):
+    class Meta:
+        model = WordFA
+
+
+class WordARType(DjangoObjectType):
+    class Meta:
+        model = WordAR
+
+
 class Query(graphene.ObjectType):
     all_chapters = graphene.List(ChapterType)
+    all_component_types = graphene.List(ComponentTypeType)
+    all_components = graphene.List(Component_Type)
+    all_texts = graphene.List(TextType)
+    all_translations = graphene.List(TranslationType)
+    all_comments = graphene.List(CommentType)
+    all_word_groups = graphene.List(WordGroupType)
+    all_words = graphene.List(WordType)
+    all_members = graphene.List(MemberType)
+    all_word_chs = graphene.List(WordCHType)
+    all_word_ens = graphene.List(WordENType)
+    all_word_des = graphene.List(WordDEType)
+    all_word_fas = graphene.List(WordFAType)
+    all_word_ars = graphene.List(WordARType)
 
     @login_required
     def resolve_all_chapters(self, info, **kwargs):
         return Chapter.objects.all()
+
+    @login_required
+    def resolve_all_component_types(self, info, **kwargs):
+        return ComponentType.objects.all()
+
+    @login_required
+    def resolve_all_components(self, info, **kwargs):
+        return Component.objects.all()
+
+    @login_required
+    def resolve_all_texts(self, info, **kwargs):
+        return Text.objects.all()
+
+    @login_required
+    def resolve_all_translations(self, info, **kwargs):
+        return Translation.objects.all()
+
+    @login_required
+    def resolve_all_comments(self, info, **kwargs):
+        return Comment.objects.all()
+
+    @login_required
+    def resolve_all_word_groups(self, info, **kwargs):
+        return WordGroup.objects.all()
+
+    @login_required
+    def resolve_all_words(self, info, **kwargs):
+        return Word.objects.all()
+
+    @login_required
+    def resolve_all_members(self, info, **kwargs):
+        return Member.objects.all()
+
+    @login_required
+    def resolve_all_word_chs(self, info, **kwargs):
+        return WordCH.objects.all()
+
+    @login_required
+    def resolve_all_word_ens(self, info, **kwargs):
+        return WordEN.objects.all()
+
+    @login_required
+    def resolve_all_word_des(self, info, **kwargs):
+        return WordDE.objects.all()
+
+    @login_required
+    def resolve_all_word_fas(self, info, **kwargs):
+        return WordFA.objects.all()
+
+    @login_required
+    def resolve_all_word_ars(self, info, **kwargs):
+        return WordAR.objects.all()
+
 
 schema = graphene.Schema(query=Query)
