c4aff5b4178ed2b1335708679c6f496aeaf2255f
Sumanthini Majutharan <sumanthini.majutharan@powercoders.org>
2019-02-24 14:09:43 +0100

    Create WordGroup Model

--- vochabular/api/admin.py
+++ vochabular/api/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from api.models import Chapter
+from api.models import WordGroup
 
 admin.site.register(Chapter)
+admin.site.register(WordGroup)
--- vochabular/api/schema.py
+++ vochabular/api/schema.py
@@ -3,16 +3,33 @@ from graphql_jwt.decorators import login_required
 
 from graphene_django.types import DjangoObjectType
 from api.models import Chapter
+from api.models import WordGroup
 
 class ChapterType(DjangoObjectType):
     class Meta:
         model = Chapter
 
+class WordGroupType(DjangoObjectType):
+    class Meta:
+        model = WordGroup
+
 class Query(graphene.ObjectType):
     all_chapters = graphene.List(ChapterType)
+    all_wordGroups = graphene.List(WordGroupType)
 
     @login_required
     def resolve_all_chapters(self, info, **kwargs):
         return Chapter.objects.all()
+
+    @login_required
+    def resolve_all_wordGroups(self, info, **kwargs):
+        return WordGroup.objects.all()
+
+
+
+
+
+
+
 
 schema = graphene.Schema(query=Query)
