89cf5dfb8f029715d8100a7fa80afae91c174890
Sumanthini Majutharan <sumanthini.majutharan@powercoders.org>
2019-02-24 16:19:04 +0100

    Set self Foreign key in Chapter Model

--- vochabular/api/models.py
+++ vochabular/api/models.py
@@ -2,15 +2,13 @@ from django.db import models
 
 class Chapter(models.Model):
     title = models.CharField(max_length=100)
-    text = models.CharField(max_length=100, null=True)
-    belongs_to = models.IntegerField(default=0)
+    belongs_to = models.ForeignKey('self', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
 class WordGroup(models.Model):
     title = models.CharField(max_length=100)
-    text = models.CharField(max_length=100, null=True)
     chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
 
     def __str__(self):
