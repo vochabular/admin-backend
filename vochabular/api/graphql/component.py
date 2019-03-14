aa97287d8da1f51782cd39a6f5ea3bf34a3cb2de
Sumanthini Majutharan <45491778+Sumanthini@users.noreply.github.com>
2019-03-14 21:36:58 +0100

    Query by Id (#16)
     - Words
     - ComponentType
     - Component
     - Comments

--- vochabular/api/graphql/component.py
+++ vochabular/api/graphql/component.py
@@ -9,20 +9,38 @@ class ComponentTypeType(DjangoObjectType):
     class Meta:
         model = ComponentType
 
+    @classmethod
+    def get_node(cls, info, id):
+        return ComponentType.objects.get(id)
+
 
 class Component_Type(DjangoObjectType):
     class Meta:
         model = Component
 
+    @classmethod
+    def get_node(cls, info, id):
+        return Component.objects.get(id)
+
 
 class ComponentQuery(graphene.AbstractType):
     component_types = graphene.List(ComponentTypeType)
+    component_type = graphene.Field(type=ComponentTypeType, id=graphene.Int())
     components = graphene.List(Component_Type)
+    component = graphene.Field(type=Component_Type, id=graphene.Int())
 
     @login_required
     def resolve_component_types(self, info, **kwargs):
         return ComponentType.objects.all()
 
     @login_required
+    def resolve_component_type(self, info, id):
+        return ComponentType.objects.get(id=id)
+
+    @login_required
     def resolve_components(self, info, **kwargs):
         return Component.objects.all()
+
+    @login_required
+    def resolve_component(self, info, id):
+        return Component.objects.get(id=id)
