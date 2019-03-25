ae957fa98152b2c244d7dad74609a1a25f34c9e7
Lukas Bischofberger <me@worx.li>
2019-03-25 21:40:50 +0100

    Fix authentication (#18)

--- vochabular/vochabular/auth.py
+++ vochabular/vochabular/auth.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import get_user_model
+from cryptography.x509 import load_pem_x509_certificate
+from cryptography.hazmat.backends import default_backend
+
 
 def user_handler(payload):
-    # TODO(worxli): get a readl username here
+    # TODO(worxli): get a real username here
     username = payload.get('email')
     User = get_user_model()
     try:
@@ -9,3 +12,11 @@ def user_handler(payload):
     except User.DoesNotExist:
         User.objects.create_user(username, username, None)
     return username
+
+
+def get_key(cert_str):
+    try:
+        cert_obj = load_pem_x509_certificate(cert_str, default_backend())
+    except Exception:
+        raise Exception("Could not parse certificate!")
+    return cert_obj.public_key()
