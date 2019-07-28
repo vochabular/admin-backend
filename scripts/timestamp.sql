4465fb9af07c20eefe2b2438cd18cdb25352b72a
David Friederich <dav.friede@gmail.com>
2019-07-28 16:02:27 +0200

    adds creationDate + modDate

---
+++ scripts/timestamp.sql
@@ -0,0 +1,69 @@
+/***
+
+Functions and triggers to insert automatically creation date and modifaction date
+
+*/
+
+CREATE OR REPLACE FUNCTION __set_creation_date() RETURNS TRIGGER AS $$
+BEGIN
+  NEW.creation_date = NOW();
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- CREATION_DATE: Set for all tables that have the creation_date column:
+DO $$
+DECLARE
+    t text;
+BEGIN
+    FOR t IN
+    SELECT table_name
+    FROM information_schema.columns
+    WHERE column_name = 'created'
+        AND table_schema = 'public'
+    LOOP
+    EXECUTE format
+    ('
+        DROP TRIGGER IF EXISTS __set_creation_date ON %I;
+        CREATE TRIGGER __set_creation_date
+        BEFORE INSERT ON %I
+        FOR EACH ROW
+        EXECUTE PROCEDURE __set_creation_date();', t, t);
+END
+LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+-- LAST_MODIFIED_DATE:
+
+CREATE OR REPLACE FUNCTION __set_modified_date() RETURNS TRIGGER AS $$
+BEGIN
+  NEW.last_modified_date = NOW();
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- CREATION_DATE: Set for all tables that have the creation_date column:
+DO $$
+DECLARE
+    t text;
+BEGIN
+    FOR t IN
+    SELECT cl.table_name
+    FROM information_schema.columns cl
+    JOIN information_schema."tables" tbl ON cl.table_name = tbl.table_name
+    WHERE tbl.table_type = 'BASE TABLE'
+    AND cl.column_name = 'updated'
+        AND cl.table_schema = 'public'
+    LOOP
+    EXECUTE format
+    ('
+        DROP TRIGGER IF EXISTS __set_modified_date ON %I;
+        CREATE TRIGGER __set_modified_date
+        BEFORE UPDATE ON %I
+        FOR EACH ROW
+        EXECUTE PROCEDURE __set_modified_date();', t, t);
+END
+LOOP;
+END;
+$$ LANGUAGE plpgsql;
