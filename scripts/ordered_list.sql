d3723a94b1d0f2f0fe1739665090e53fcf173b2f
David Friederich <dav.friede@gmail.com>
2019-11-19 20:50:37 +0100

    Adds "belongs_to" to orderd list trigger to handle orderings for root components for each chapter

--- scripts/ordered_list.sql
+++ scripts/ordered_list.sql
@@ -15,6 +15,7 @@ CREATE OR REPLACE FUNCTION __reorder_ordered_list() RETURNS TRIGGER AS $$
 declare
   _column_name text;
   _self_ref_column_name text;
+  _self_ref_belongs_to text;
   _old_order int;
   _new_order int;
   _is_lowered_in_order bool;
@@ -26,6 +27,7 @@ BEGIN
     END IF;
    _column_name := TG_ARGV[0];
    _self_ref_column_name := TG_ARGV[1];
+   _self_ref_belongs_to := TG_ARGV[2];
      
     -- INSERT
     IF  (TG_OP = 'INSERT') THEN
@@ -36,8 +38,9 @@ BEGIN
         UPDATE %1$I.%2$I SET %3$I = %3$I + 1 
         WHERE %3$I >= $1.%3$I
 		AND %4$I IS NOT DISTINCT FROM $1.%4$I
+        AND %5$I = $1.%5$I
 		AND id <> $1.id 
-        ', TG_TABLE_SCHEMA, TG_TABLE_NAME, _column_name, _self_ref_column_name) USING new;
+        ', TG_TABLE_SCHEMA, TG_TABLE_NAME, _column_name, _self_ref_column_name, _self_ref_belongs_to) USING new;
     END IF;
 
     -- UPDATE
@@ -50,16 +53,16 @@ BEGIN
     	EXECUTE format('SELECT $1.%1$I', _column_name) using old into _old_order;
     	EXECUTE format('SELECT $1.%1$I', _column_name) using new into _new_order;
     	IF _new_order < _old_order then
-            EXECUTE format('UPDATE %1$I.%2$I SET %3$I = %3$I + 1 WHERE id <> $1.id AND %3$I >= $1.%3$I AND %3$I < $2.%3$I AND %4$I IS NOT DISTINCT FROM $2.%4$I', TG_TABLE_SCHEMA, TG_TABLE_NAME, _column_name, _self_ref_column_name) using new, old, TG_RELID;
+            EXECUTE format('UPDATE %1$I.%2$I SET %3$I = %3$I + 1 WHERE id <> $1.id AND %3$I >= $1.%3$I AND %3$I < $2.%3$I AND %4$I IS NOT DISTINCT FROM $2.%4$I AND %5$I = $1.%5$I', TG_TABLE_SCHEMA, TG_TABLE_NAME, _column_name, _self_ref_column_name, _self_ref_belongs_to) using new, old, TG_RELID;
         -- We don't need to do anything if the order stays the same!
         ELSIF _new_order > _old_order then
-            EXECUTE format('UPDATE %1$I.%2$I SET %3$I = %3$I - 1 WHERE id <> $1.id AND %3$I <= $1.%3$I AND %3$I > $2.%3$I AND %4$I IS NOT DISTINCT FROM $2.%4$I', TG_TABLE_SCHEMA, TG_TABLE_NAME, _column_name, _self_ref_column_name) using new, old, TG_RELID;
+            EXECUTE format('UPDATE %1$I.%2$I SET %3$I = %3$I - 1 WHERE id <> $1.id AND %3$I <= $1.%3$I AND %3$I > $2.%3$I AND %4$I IS NOT DISTINCT FROM $2.%4$I AND %5$I = $1.%5$I', TG_TABLE_SCHEMA, TG_TABLE_NAME, _column_name, _self_ref_column_name, _self_ref_belongs_to) using new, old, TG_RELID;
         END IF;
     END IF;
 
     -- DELETE
     IF  (TG_OP = 'DELETE') THEN
-        EXECUTE format('UPDATE %1$I.%2$I SET %3$I = %3$I - 1 WHERE %3$I > $1.%3$I AND %4$I IS NOT DISTINCT FROM $1.%4$I', TG_TABLE_SCHEMA, TG_TABLE_NAME, _column_name, _self_ref_column_name) using OLD;
+        EXECUTE format('UPDATE %1$I.%2$I SET %3$I = %3$I - 1 WHERE %3$I > $1.%3$I AND %4$I IS NOT DISTINCT FROM $1.%4$I AND %5$I = $1.%5$I', TG_TABLE_SCHEMA, TG_TABLE_NAME, _column_name, _self_ref_column_name, _self_ref_belongs_to) using OLD;
         RETURN OLD;
     END IF;
     RETURN NEW;
@@ -90,7 +93,7 @@ BEGIN
         CREATE TRIGGER __reorder_ordered_list
         AFTER INSERT OR DELETE OR UPDATE OF %2$I ON %1$I
         FOR EACH ROW
-        EXECUTE PROCEDURE __reorder_ordered_list(%2$I, %4$I);
+        EXECUTE PROCEDURE __reorder_ordered_list(%2$I, %4$I, %3$I);
         ', t[1], t[2], t[3], t[4]);
 END
 LOOP;
